7d7426001a112f29adcb7998eff7b571d5a9ee8f
tomfantova <tom.fantova@gmail.com>
2022-12-12 20:19:09 +0100

    backJ1

--- models/users.js
+++ models/users.js
@@ -1,26 +1,28 @@
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 
 const usersSchema = mongoose.Schema({
-    firstname: String,
-    lastname: String,
-    email: String,
-    password: String,
-    token: String,
-    preferences: {String},
-    bookmarks: {
-        interviews: [{ type: mongoose.Schema.Types.ObjectId, ref: 'interviews' }],
-        generalities: [{ type: mongoose.Schema.Types.ObjectId, ref: 'general' }],
-        products: [{ type: mongoose.Schema.Types.ObjectId, ref: 'products' }],
-        labels: [{ type: mongoose.Schema.Types.ObjectId, ref: 'labels' }],
+  firstname: String,
+  lastname: String,
+  email: String,
+  password: String,
+  token: String,
+  preferences: { String },
+  bookmarks: {
+    interviews: [{ type: mongoose.Schema.Types.ObjectId, ref: "interviews" }],
+    generalities: [{ type: mongoose.Schema.Types.ObjectId, ref: "general" }],
+    products: [{ type: mongoose.Schema.Types.ObjectId, ref: "products" }],
+    labels: [{ type: mongoose.Schema.Types.ObjectId, ref: "labels" }],
+  },
+  lists: [
+    {
+      name: String,
+      date: Date,
+      active: Boolean,
+      categories: [],
     },
-    lists: [{
-        name: String,
-        date: Date,
-        active: Boolean,
-        categories: {}, 
-    }]
-})
+  ],
+});
 
-const User = mongoose.model('users', usersSchema)
+const User = mongoose.model("users", usersSchema);
 
-module.exports = User
+module.exports = User;
