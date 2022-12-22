073b171d58f50dc580c46465e65c86fe4df634a7
Joé <milcentjoe@gmail.com>
2022-12-22 18:53:09 +0100

    joé

--- models/users.js
+++ models/users.js
@@ -1,31 +1,43 @@
 const mongoose = require("mongoose");
 
-const usersSchema = mongoose.Schema({
-    firstname: String,
-    lastname: String,
-    email: String,
-    password: String,
-    token: String,
-    preferences: {
-        type: Number,
-    },
-    bookmarks: [{
-        type: mongoose.Schema.Types.ObjectId, ref: "guides"
-    }],
-    lists: [{
-        name: String,
-        date: Date,
-        active: Boolean,
-        categories: [{
-            name: String,
-            image: String,
-            items: [{
-                name: String,
-                active: Boolean,
-            }],
-        }],
-    }]
+const preferencesSchema = mongoose.Schema({
+    type: Number,
+    dietetique: Number,
+    bilan: Number,
+    ethique: Number,
+    local: Number,
+    agriculture: Number,    
 })
+
+const itemSchema = mongoose.Schema({
+    name: String,
+    active: Boolean,
+})
+
+const categorySchema = mongoose.Schema({
+    name: String,
+    image: String,
+    items: [itemSchema],
+})
+
+const listSchema = mongoose.Schema({
+    name: String,
+    date: String,
+    active: Boolean,
+    categories: [categorySchema],
+  })
+
+const usersSchema = mongoose.Schema({
+  firstname: String,
+  lastname: String,
+  email: String,
+  password: String,
+  token: String,
+  date: Date,
+  preferences: preferencesSchema,
+  bookmarks: [{ type: mongoose.Schema.Types.ObjectId, ref: "guides" }],
+  lists: [listSchema]
+});
 
 const User = mongoose.model("users", usersSchema);
 
--- routes/users.js
+++ routes/users.js
@@ -4,6 +4,10 @@ const router = express.Router()
 const User = require('../models/users')
 
 const { formatName } = require('../modules/formatName')
+const { checkBody } = require("../modules/checkBody");
+
+const uid2 = require("uid2");
+const bcrypt = require("bcrypt");
 
 
 
@@ -17,27 +21,65 @@ router.get('/all', async (req, res) => {
 
 // POST Nouvel utilisateur
 
-router.post('/new', async (req, res) => {
-
+router.post("/new", async (req, res) => {
   // Créer ici le check du formulaire et des utilisateurs déjà existants
+  if (!checkBody(req.body, ["firstname", "lastname", "email", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+  console.log(req.body);
+  User.findOne({ email: { $regex: new RegExp(req.body.email, "i") } }).then(
+    (emailData) => {
+      if (emailData === null) {
+        const hash = bcrypt.hashSync(req.body.password, 10);
 
-  const newUser = new User({
-    firstname: formatName(req.body.firstname),
-    lastname: formatName(req.body.lastname),
-    email: req.body.email.toLowerCase(),
-    password: req.body.password, // Changer cette ligne : utiliser bcrypt
-    token: 'À DÉFINIR', // Changer cette ligne : utiliser uid2
-    preferences: {
-      type: req.body.preferences.type, // Je rajouterai les autres préférences, pour l'instant on laisse seulement "type"
-    },
-    bookmarks: [],
-    lists: [],
-  })
+    const newUser = new User({
+      firstname: formatName(req.body.firstname),
+      lastname: formatName(req.body.lastname),
+      email: req.body.email.toLowerCase(),
+      date: new Date(),
+      password: hash,
+      token: uid2(32),
+      preferences: {
+        type: req.body.preferences.type,
+        dietetique: req.body.preferences.dietetique,
+        bilan: req.body.preferences.bilan,
+        ethique: req.body.preferences.ethique,
+        local: req.body.preferences.local,
+        agriculture: req.body.preferences.agriculture,
+      },
+      bookmarks: [],
+      lists: [],
+    });
+    console.log('entered')
+    newUser.save().then(savedUserData => {
+      res.json({ result: true, newUser: savedUserData })
+    })
+  } else {
+    // Utilisateur déjà existant
+    res.json({ result: false, error: "User already exist" });
+  }
+}
+  );
+});
 
-  const savedUserData = await newUser.save()
-  res.json({ result: true, newUser: savedUserData })
 
-})
+// POST SE CONNECTER
+
+router.post("/signin", (req, res) => {
+  if (!checkBody(req.body, ["email", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
+    return;
+  }
+
+  User.findOne({ email: req.body.email }).then((data) => {
+    if (data && bcrypt.compareSync(req.body.password, data.password)) {
+      res.json({ result: true, token: data.token });
+    } else {
+      res.json({ result: false, error: "User not found or wrong password" });
+    }
+  });
+});
 
 
 // DELETE Tous les utilisateurs
